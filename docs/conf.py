f128ea777abe8699efa59f323e610c63e7acce08
christineschottmueller <c.schottmueller@tu-braunschweig.de>
2025-06-05 13:55:45 +0200

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,5 +43,6 @@ html_context = {
   'display_github': True,
   'github_user': 'christineschottmueller',
   'github_repo': 'x-marsh',
-  'github_version': '/tree/master/docs/'
+  'github_url' = "https://github.com/christineschottmueller/x-marsh/tree/master"
+  #'github_version': 'master'
 }
